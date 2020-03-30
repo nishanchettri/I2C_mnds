82b19c12b51b4593bc691f997b4dccc719b3d5d8
Nishan Chettri <35814272+nishanchettri@users.noreply.github.com>
2020-03-30 12:00:18 +0530

    Add files via upload

---
+++ i2cMNDS/master/master.ino
@@ -0,0 +1,14 @@
+#include <Wire.h>
+
+int x = 0;
+void setup() 
+{
+  Wire.begin(); 
+}
+void loop() {
+  Wire.beginTransmission(10); // transmit to device #10
+  Wire.write(x);              // sends x 
+  Wire.endTransmission();    // stop transmitting
+  x++; // Increment x
+  delay(500);
+}
---
+++ i2cMNDS/slave/slave.ino
@@ -0,0 +1,17 @@
+#include <Wire.h>
+
+int x = 0;
+void setup() 
+{
+  Serial.begin(9600);
+  Wire.begin(10); 
+  Wire.onReceive(rx);
+}
+void rx(int size) 
+{
+  x = Wire.read();    // read one character from the I2C
+}
+void loop()
+{
+  Serial.println(x);
+}
